31d6a977f5fec8038c7bc0b5ac3d920313b22466
darianmavgo <d@mavgo.com>
2022-07-08 11:39:08 -0700

    https://stackoverflow.com/questions/72915138/how-to-fix-database-is-locked-when-no-concurrent-threads-are-involved-golang/72915678#72915678

--- runsql.go
+++ runsql.go
@@ -9,31 +9,43 @@ import (
 )
 
 func main() {
-	dbPath := "/Users/darianhickman/Documents/wc_study/history.db"
-
-	engine := sqlx.MustConnect("sqlite3", dbPath)
+	// dbPath := "file:/Users/darianhickman/Documents/wc_study/history.db?cache=shared"
+	// dbPath := "file:/Users/darianhickman/Documents/wc_study/history.db"
+	db := sqlx.MustConnect("sqlite3", dbPath)
+	defer db.Close()
+	// db.SetMaxOpenConns(1)
 
 	// qry := `select list_folder||'/'|| sql_file as script from run_list where 'order' > 0 ORDER BY 'order'; `
 	qry := `select list_folder||'/'|| sql_file as script from run_list 
 	where run_order > 0 
 	ORDER BY run_order;`
 
-	scripts, err := engine.Query(qry)
+	scripts, err := db.Query(qry)
 	if err != nil {
-		println("queary failed ", qry)
+		println("script qry failed ", qry)
 	}
+	defer scripts.Close()
+	var files []string
 	var file string
+
+	// Giving up on executing sql scripts while reading the query of scripts.
 	for scripts.Next() {
 		err = scripts.Scan(&file)
 		if err != nil {
 			println(err)
+		} else {
+			files = append(files, file)
 		}
+	}
+
+	for _, file := range files {
+
 		println(file[len(file)-80:])
 		sqlScript, err := ioutil.ReadFile(file)
 		if err != nil {
 			println("reading script file failed\n", file)
 		}
-		if _, err := engine.Exec(string(sqlScript)); err != nil {
+		if _, err := db.Exec(string(sqlScript)); err != nil {
 			fmt.Println(string(sqlScript[:80]))
 			fmt.Println(err)
 
