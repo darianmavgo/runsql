dca38ad3c7360ed9655f93548dec3c92d9ec44a1
darianmavgo <d@mavgo.com>
2022-07-28 10:02:53 -0700

    adding support for load table path.

--- runsql.go
+++ runsql.go
@@ -12,10 +12,13 @@ import (
 
 // #run a script to clean for next run.
 // func clean_run(script)
+type Config struct {
+	loadtablepath string
+}
 
 func main() {
 
-	dbPath, err := filepath.Abs(os.Args[1])
+	dbPath, err := filepath.Abs(os.Args[1]) // passing in settings.db
 	if err != nil {
 		log.Fatal("Failed db string ", os.Args[1], err)
 	}
@@ -24,11 +27,15 @@ func main() {
 	db := sqlx.MustConnect("sqlite3", dbPath)
 	defer db.Close()
 
+	cfgqry := `select loadtablepath from config;`
+	c := Config{}
+	db.Get(&c, cfgqry)
+	// now I have the config so time to run backtest against the right file.
+
 	// qry := `select list_folder||'/'|| sql_file as script from run_list where 'order' > 0 ORDER BY 'order'; `
 	qry := `select list_folder||'/'|| sql_file as script, run_order from run_list 
 	where run_order > 0 
 	ORDER BY run_order;`
-
 	scripts, err := db.Query(qry)
 	if err != nil {
 		println("script qry failed ", qry)
@@ -49,6 +56,14 @@ func main() {
 		}
 	}
 
+	loadDbPath, err := filepath.Abs(`/Users/darianhickman/Documents/wc_study/history_large.db`)
+	// loadDbPath, err := filepath.Abs(c.loadtablepath)
+	if err != nil {
+		log.Fatalln(`Couldnot create path`, err, c.loadtablepath)
+	}
+	log.Println("loadDbPath ", loadDbPath)
+
+	target_db := sqlx.MustConnect("sqlite3", loadDbPath)
 	for idx, file := range files {
 		log.Print(orders[idx], " ")
 		if len(file) > 80 {
@@ -64,7 +79,7 @@ func main() {
 		if err != nil {
 			log.Println("Fail: ", file, err)
 		}
-		if result, err := db.Exec(string(sqlScript)); err != nil {
+		if result, err := target_db.Exec(string(sqlScript)); err != nil {
 			log.Println(string(sqlScript[:80]))
 			log.Println(err)
 
